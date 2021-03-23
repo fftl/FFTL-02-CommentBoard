77cd97adba7b5272fb68d7ddea0bdde3c61338dd
fftl <fftl.0710@gmail.com>
2021-03-23 12:15:40 +0900

    rebuild: backend project

--- fftl-02-back/src/app.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { LocalAuthGuard } from './auth/local-auth.guard';
-import { AuthService } from './auth/auth.service';
-
-@Controller()
-export class AppController {
-}
---
+++ fftl-02-back/src/test/java/fftlproject/AccountBook/AccountBookApplicationTests.java
@@ -0,0 +1,13 @@
+package fftlproject.AccountBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
