09b0ec4d10d85eb3e5625e80a00530b8069d7725
fftl <fftl.0710@gmail.com>
2021-03-24 23:50:55 +0900

    update_back: upload controller

---
+++ fftl-02-back/src/main/java/fftlproject/controller/userController.java
@@ -0,0 +1,12 @@
+package fftlproject.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@RequestMapping("/user/")
+@Controller
+public class userController {
+
+}
