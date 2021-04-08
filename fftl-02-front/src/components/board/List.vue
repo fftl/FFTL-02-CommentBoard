13afde4348c965cbc411c696a4f4658873006b52
fftl <fftl.0710@gmail.com>
2021-04-08 17:45:50 +0900

    update: front, basic board set

---
+++ fftl-02-front/src/components/board/List.vue
@@ -0,0 +1,45 @@
+<template>
+	<div>
+		<h2>게시판 리스트</h2>
+        <button class="goWrite" @click="goWrite">글 작성하기</button>
+        <paginated-list :list-array="pageArray" />
+	</div>
+</template>
+
+<script>
+import PaginatedList from './PaginatedList'
+export default {
+    components: {
+        PaginatedList
+    },
+    data(){
+        return {
+            pageArray: []
+        }
+    },
+    mounted(){
+        this.getList()
+    }
+    ,methods: {
+        getList(){
+            this.$http.get("http://127.0.0.1:3000/board")
+            .then((res)=>{
+				console.log(res.data);
+                this.pageArray = res.data;
+			})
+			.then((err)=>{
+				console.log(err);
+			})
+        }
+        ,goWrite(){
+			this.$router.push({path:'/board/write'}); //추가한 상세페이지 라우터
+        }
+    }
+}
+</script>
+
+<style scoped>
+.goWrite{
+      float: right;
+}
+</style>
---
+++ fftl-02-front/src/routes/index.js
@@ -0,0 +1,35 @@
+import {createRouter, createWebHistory} from 'vue-router';
+import List from '@/components/board/List'; //게시판 리스트 컴포넌트 호출
+import Write from '@/components/board/Write'; //게시판 리스트 컴포넌트 호출
+import View from '@/components/board/View'; //게시판 리스트 컴포넌트 호출
+import Join from '@/components/user/Join';
+
+const routerHistory = createWebHistory()
+
+const router = createRouter(({
+    history: routerHistory,
+    routes:[
+		{
+			path:'/'
+			,component:List
+		}
+		,{
+			path:'/board/list'
+			,component:List
+		}
+        ,{
+			path:'/board/write'
+			,component:Write
+		},
+        {
+			path:'/board/view'
+			,component:View
+		},
+		{
+			path:'/user/join'
+			,component:Join
+		}
+	]
+}))
+
+export default router
