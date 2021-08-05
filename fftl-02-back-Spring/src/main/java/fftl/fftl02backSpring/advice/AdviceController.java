55935e1b6fd4c5923c6539dac1d9a507453cca94
fftl <fftl.0710@gmail.com>
2021-08-05 16:12:20 +0900

    update

--- fftl-02-back-Spring/src/main/java/fftl/fftl02backSpring/advice/AdviceController.java
+++ fftl-02-back-Spring/src/main/java/fftl/fftl02backSpring/advice/AdviceController.java
@@ -8,15 +8,13 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-
 @RequiredArgsConstructor
 @ControllerAdvice
 @RestController
 public class AdviceController {
 
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<FailResponse> basicException(HttpServletRequest request, Exception e){
+    protected ResponseEntity<FailResponse> basicException(){
         return new ResponseEntity<>(new FailResponse("fail", "알 수 없는 오류입니다."), HttpStatus.OK);
     }
 
