6031c3813ef80e21601e1b317298c4e0db3033bb
fftl <fftl.0710@gmail.com>
2021-08-09 19:23:01 +0900

    fix untracked files

--- fftl-02-back-Spring/src/main/java/fftl/fftl02backSpring/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package fftl.fftl02backSpring.repository;
-
-import fftl.fftl02backSpring.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
