a3afc60fd8d76a8fbf022f906fc25f54195b344e
fftl <fftl.0710@gmail.com>
2021-10-26 01:09:42 +0900

    test code study

--- fftl-02-back-Spring/src/test/java/fftl/fftl02backSpring/service/BoardServiceTest.java
+++ fftl-02-back-Spring/src/test/java/fftl/fftl02backSpring/service/BoardServiceTest.java
@@ -3,6 +3,7 @@ package fftl.fftl02backSpring.service;
 import fftl.fftl02backSpring.entity.Board;
 import fftl.fftl02backSpring.repository.BoardRepository;
 import fftl.fftl02backSpring.request.SaveBoardDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,6 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -26,6 +28,20 @@ class BoardServiceTest {
     @Mock
     private BoardRepository boardRepository;
 
+    static List<Board> boards;
+    static Board board1;
+    static Board board2;
+
+    @BeforeEach
+    void init() {
+        boards = new ArrayList<>();
+        board1 = Board.builder().title("제목!").bregdate("2021-03-01").content("내용입니다!").nickname("작성자에요!").bid(1L).uid(1L).build();
+        board2 = Board.builder().title("제목222").bregdate("2021-03-02").content("내용입니다!").nickname("작성자22!").bid(2L).uid(2L).build();
+
+        boards.add(board1);
+        boards.add(board2);
+    }
+
     @Test
     void saveBoard() {
 
@@ -36,7 +52,6 @@ class BoardServiceTest {
         //when
         Board board = boardService.saveBoard(saveBoardDto);
 
-
         //then
         assertThat(board.getNickname()).isEqualTo("nickname");
     }
@@ -45,21 +60,31 @@ class BoardServiceTest {
     void getAllBoard() {
 
         //given
-        List<Board> boards = new ArrayList<>();
-        Board board1 = Board.builder().title("제목!").bregdate("2021-03-01").content("내용입니다!").nickname("작성자에요!").bid(1L).uid(1L).build();
-        boards.add(board1);
         given(boardRepository.findAll()).willReturn(boards);
 
         //when
         List<Board> findAll = boardService.getAllBoard();
 
         //then
+        assertThat(findAll.size()).isEqualTo(boards.size());
         assertThat(findAll.get(0).getNickname()).isEqualTo(board1.getNickname());
+        assertThat(findAll.get(1).getNickname()).isEqualTo(board2.getNickname());
 
     }
 
     @Test
     void getOneBoard() {
+
+        //given
+        given(boardRepository.findById(any())).willReturn(Optional.ofNullable(board1));
+
+        //when
+        Board board = boardService.getOneBoard(1L);
+
+        //then
+        assertThat(board.getNickname()).isEqualTo(board1.getNickname());
+
+
     }
 
 
